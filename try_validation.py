367b99d7c68d118f543967b89a55a088fc258f30
Chungsang Tom Lam <erinata@gmail.com>
2020-10-22 13:18:09 -0400

    try validation

---
+++ try_validation.py
@@ -0,0 +1,51 @@
+import pandas
+from sklearn import linear_model
+from sklearn.model_selection import train_test_split 
+from matplotlib import pyplot as plt
+from sklearn import metrics
+
+dataset = pandas.read_csv("regression_dataset.csv")
+# print(dataset)
+
+target = dataset.iloc[:,0].values
+# print(target)
+
+data = dataset.iloc[:,3:9].values
+# print(data)
+
+data_training, data_test, target_training, target_test = train_test_split(data, target, test_size=0.25, random_state=0)
+
+# print(data.shape)
+# print(target.shape)
+
+# print(data_training.shape)
+# print(data_test.shape)
+
+# print(target_training.shape)
+# print(target_test.shape)
+
+machine = linear_model.LinearRegression()
+machine.fit(data_training, target_training)
+
+results = machine.predict(data_test)
+
+print(results)
+
+plt.scatter(target_test, results)
+plt.xlabel("target_test")
+plt.ylabel("machine_predict")
+
+plt.savefig("scatter_test.png")
+
+print(metrics.r2_score(target_test,results))
+
+
+
+
+
+
+
+
+
+
+
